e88bcb3c828b4b1ce807708c27da3e9940d7f968
milan <pperisic.milan@gmail.com>
2022-07-11 21:56:45 +0200

    Adding todos, make item without empty spaces, and clear input field

---
+++ app.js
@@ -0,0 +1,26 @@
+const addForm = document.querySelector('form.add');
+const todoList = document.querySelector('.todos');
+
+const template = todo => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="fa-solid fa-trash-can delete"></i>
+    </li>
+  `;
+
+  todoList.innerHTML += html;
+};
+
+
+
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todoItem = addForm.add.value.trim();
+
+  if(todoItem){
+    template(todoItem);
+    addForm.reset();
+  }
+
+});
