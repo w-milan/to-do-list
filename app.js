53e09f6c7b04cf657a5d41a8e4f8134bfd47f7e9
milan <pperisic.milan@gmail.com>
2022-08-30 17:26:35 +0200

    making search

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('form.add');
 const todoList = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 // template html
 const template = todo => {
@@ -18,16 +19,39 @@ addForm.addEventListener('submit', e => {
   e.preventDefault();
   const todoItem = addForm.add.value.trim();
 
-  if(todoItem){
+  if (todoItem) {
     template(todoItem);
     addForm.reset();
   }
 });
 
-
 // delete to-do item
 todoList.addEventListener('click', e => {
-  if(e.target.classList.contains('delete')){
+  if (e.target.classList.contains('delete')) {
     e.target.parentElement.remove();
   }
+});
+
+const filterTodos = (term) => {
+  Array.from(todoList.children)
+    .filter((todo) => {
+      return !todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.add('filtered');
+    })
+
+    Array.from(todoList.children)
+    .filter((todo) => {
+      return todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      todo.classList.remove('filtered');
+    })
+};
+
+// keyup event
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
